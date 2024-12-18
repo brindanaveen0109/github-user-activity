f066c764aa196f62cf565794c252961ef55c06fb
Brinda Naveen <brindanaveen0109@gmail.com>
2024-12-18 12:22:45 +0530

    System Exit codes added

--- github_user_activity/api_req.py
+++ github_user_activity/api_req.py
@@ -1,4 +1,5 @@
 import requests
+import sys
 
 def fetch_github_activity(username): 
     url = f"https://api.github.com/users/{username}/events"
@@ -9,6 +10,8 @@ def fetch_github_activity(username):
             return response.json()
         elif response.status_code == 404:
             print("User not found")
+            sys.exit(0)
     except requests.exceptions.RequestException as e:
         print(f"Error: Unable to connect. {e}")
+        sys.exit(1) 
     return []
