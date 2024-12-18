8a8bfc1c2996baa33e88b42ec0d636f280d5ef82
Brinda Naveen <brindanaveen0109@gmail.com>
2024-12-18 10:59:52 +0530

    the position or url changed

--- github_user_activity/api_req.py
+++ github_user_activity/api_req.py
@@ -1,14 +1,14 @@
 import requests
 
-def fetch_github_activity(username):
+def fetch_github_activity(username): 
     url = f"https://api.github.com/users/{username}/events"
     try:
         response = requests.get(url)
         if response.status_code == 200:
             print("HTTP request successful")
+            return response.json()
         elif response.status_code == 404:
-            print("User not found.")
+            print("User not found")
     except requests.exceptions.RequestException as e:
         print(f"Error: Unable to connect. {e}")
-
     return []
