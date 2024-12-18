cdb1f33d4813a6a1a2fda3a0ce0b31a4bc4c49ed
Brinda Naveen <brindanaveen0109@gmail.com>
2024-12-18 12:42:18 +0530

    argparse is used instead of sys

--- github_user_activity/cli.py
+++ github_user_activity/cli.py
@@ -1,16 +1,18 @@
-import sys
+import argparse
 from github_user_activity.api_req import fetch_github_activity
 from github_user_activity.formatter import format_activity
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: python -m github_user_activity <username>")
-        sys.exit(2)
-        
+    parser = argparse.ArgumentParser(
+        description="This script fetches and displays recent activity for a GitHub user. \n"
+                    "It shows events such as commits, issues, pull requests, and more from their GitHub account. \n"
+                    "To execute the script, follow the command : python github_user_activity.cli <username>"
+    )    
+    parser.add_argument('username', type=str, help ="Github username to fetch activity for. For example: brindanaveen0109" )
+    args = parser.parse_args()
 
-    username = sys.argv[1]
-    print(f"Fetching recent activity for GitHub user: {username}...\n")
-    events = fetch_github_activity(username)
+    print(f"Fetching recent activity for GitHub user: {args.username}...\n")
+    events = fetch_github_activity(args.username)
     print(format_activity(events))
-
+    
 if __name__ == "__main__":
     main()
